9c9fa2f4a09e2ac4acd92828c5970973c8f63175
safak <safakyilmaz57@hotmail.com>
2022-08-17 09:34:16 +0300

    obserable-worker

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,13 +15,7 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'angular-obserable-worker'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-obserable-worker');
-  });
-
+  
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {fromWorkerPool} from "observable-webworker";
+import {FileHashPayload} from "./file-hasher.interface";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-obserable-worker';
+
+  public hashFiles($event:any): void {
+    const files: File[] = Array.from($event.target.files);
+    console.log(`files`, files);
+    fromWorkerPool<File, FileHashPayload>(
+      () => new Worker(new URL(`./file-hasher.worker.ts`, import.meta.url), { type: 'module' }),
+      files,
+    ).subscribe((hashPayload: FileHashPayload) => {
+      console.log('Hashed file', hashPayload.filename, hashPayload.hash);
+    });
+  }
+
 }
